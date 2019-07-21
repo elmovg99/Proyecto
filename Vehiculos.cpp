352a23f772801684e548990d2cf180f8004d7fbc
Erick Gil <erickgildardoas@gmail.com>
2019-07-20 20:29:02 -0500

    agregado y mostrado
    funciona

--- Vehiculos.cpp
+++ Vehiculos.cpp
@@ -2,6 +2,9 @@
 
 #include <iostream>
 #include <stdlib.h>
+#include "string.h"
+#include "conio.h"
+#include "stdio.h"
 
 using namespace std;
 
@@ -41,72 +44,50 @@ class Carro: public Vehiculo{
 		string modelo;
 		string marca;
 	public:
-		Carro(){
-			numLlantas=0;
-			capacidad=0;
-			motor="x";
-			modelo="x";
-			marca="x";
+		Carro *sig;
+		Carro *ant;
+		Carro():Vehiculo(){
+			this->numLlantas=0;
+			this->capacidad=0;
+			this->motor="x";
+			this->modelo="x";
+			this->marca="x";
+			this->sig=NULL;
+			this->ant=NULL;
 		}
 		void setmodelo(string d){
-			modelo=d;
+			this->modelo=d;
 		}
 		void setmarca(string e){
-			marca=e;
+			this->marca=e;
 		}
 		string getmodelo(){
-			return modelo;
+			return this->modelo;
 		}
 		string getmarca(){
-			return marca;
+			return this->marca;
 		}
 };
 
-class Nodo:public Vehiculo{
-	public:
-		Nodo *sig;
-		Nodo *ant;
-		Carro *datos;
-		Nodo(){
-			this->sig=NULL;
-			this->datos=NULL;
-		}
-		Nodo(Carro *c){
-			this->sig=NULL;
-			this->datos=c;
-		}
-		int getnumLlantas(Nodo *lista){
-			return lista->datos->numLlantas;
-		}
-		int getcapacidad(Nodo *lista){
-			return lista->datos->capacidad;
-		}
-		string getmotor(Nodo *lista){
-			return lista->datos->motor;
-		}
-		string getmarca(Nodo *lista){
-			return lista->datos->marca;
-		}
-		string getmodelo(Nodo *lista){
-			return lista->datos->modelo;
-		}
-};
+void registrar(Carro *);
+void mostrar(Carro *);
 
 class Lista{
 	private:
-		Nodo *lista;
+		Carro *lista;
 	public:
 		Lista(){
 			lista=NULL;
 		}
-		public: void agregarNodo(Carro *c){
-			Nodo *nuevo = new Nodo(c);
+		public: void agregarCarro(){
+			Carro *nuevo = new Carro();
+			registrar(nuevo);
 			if(lista==NULL){
 				lista=nuevo;
 			}
 			else{
-				Nodo *aux=new Nodo();
-				Nodo *ant= new Nodo();
+				Carro *aux=new Carro();
+				Carro *ant= new Carro();
 				aux=lista;
 				while(aux->sig!=NULL){
 					ant=aux;
@@ -118,78 +99,80 @@ class Lista{
 		}
 			
 		void recorrerLista(){
+			char ind;
 			if(lista==NULL)
-				cout<<"Lista vacia"<<endl;
+				cout<<"  Lista vacia"<<endl;
 			else{
-				Nodo *aux = new Nodo();
+				Carro *aux = new Carro();
 				aux=lista;
-				while(aux!=NULL){
-					cout<<endl<<"Marca: "<<aux->getmarca(aux)<<endl;
-					cout<<"Modelo: "<<aux->getmodelo(aux)<<endl;
-					cout<<"Capacidad de pasajeros: "<<aux->getcapacidad(aux)<<endl;
-					cout<<"Motor: "<<aux->getmotor(aux)<<endl;
-					cout<<"Numero de llantas: "<<aux->getnumLlantas(aux)<<endl;
-					cout<<endl;
-					aux=aux->sig;
-				}
+				do{
+					system("cls");
+					cout << "  A-Para atras"<< endl;
+					cout << "  D-Para delante"<< endl;
+					cout << "  S-Salir/Seleccionar"<< endl << endl;
+					mostrar(aux);
+					ind=getche();
+					aux=(((ind=='a'||ind=='A')&&aux->ant!=NULL)?aux->ant:((ind=='d'||ind=='D')&&aux->sig!=NULL)?aux->sig:aux);
+				}while(ind != 'S' && ind != 's');
 			}
 		}
-	public: void eliminarNodo(int x){
-		Nodo *aux=lista;
-		Nodo *ant=NULL;
-		while((aux!=NULL)&&(aux->getNum()!=x)){
-			ant=aux;
-			aux=aux->sig;
-		}
-		if(aux!=NULL){
-			if(ant!=NULL){
-				ant->sig=aux->sig;
-				aux->sig->ant=aux->ant;
-			}
-			else
-				lista=aux->sig;
-			free(aux);
-			cout<<"Nodo eliminado"<<endl<<endl;
-		}
-		else
-			cout<<"El valor no esta en la lista"<<endl<<endl;
-	}
-	public: void buscarValor(int x){
-		Nodo *aux = new Nodo();
-		int editar=0;
-		aux=lista;
-		while(aux!=NULL&&aux->getNum()!=x)
-			aux=aux->sig;
-		if(aux==NULL)
-			cout<<endl<<"El valor no esta en la lista"<<endl<<endl;
-		else{
-			cout<<endl<<"	Valor encontrado"<<endl<<endl;
-			cout<<"Modifica valor?(Si=1, No=0)\nOpcion: "; 
-			cin>>editar;
-			cout<<endl;
-			if(editar==1){
-				cout<<"Nuevo valor: ";
-				cin>>x;
-				cout<<endl;
-				aux->setNum(x);
-			}
-		}
-	}
+		
+//	public: void eliminarNodo(int x){
+//		Nodo *aux=lista;
+//		Nodo *ant=NULL;
+//		while((aux!=NULL)&&(aux->getNum()!=x)){
+//			ant=aux;
+//			aux=aux->sig;
+//		}
+//		if(aux!=NULL){
+//			if(ant!=NULL){
+//				ant->sig=aux->sig;
+//				aux->sig->ant=aux->ant;
+//			}
+//			else
+//				lista=aux->sig;
+//			free(aux);
+//			cout<<"Nodo eliminado"<<endl<<endl;
+//		}
+//		else
+//			cout<<"El valor no esta en la lista"<<endl<<endl;
+//	}
+//	public: void buscarValor(int x){
+//		Nodo *aux = new Nodo();
+//		int editar=0;
+//		aux=lista;
+//		while(aux!=NULL&&aux->getNum()!=x)
+//			aux=aux->sig;
+//		if(aux==NULL)
+//			cout<<endl<<"El valor no esta en la lista"<<endl<<endl;
+//		else{
+//			cout<<endl<<"	Valor encontrado"<<endl<<endl;
+//			cout<<"Modifica valor?(Si=1, No=0)\nOpcion: "; 
+//			cin>>editar;
+//			cout<<endl;
+//			if(editar==1){
+//				cout<<"Nuevo valor: ";
+//				cin>>x;
+//				cout<<endl;
+//				aux->setNum(x);
+//			}
+//		}
+//	}
 };
 
 void registrar(Carro *);
 
 int main(){
 	int opcion;
-	Carro *carro = new Carro();
 	Lista *list = new Lista();
 	do{
-		cout<<"1)Agregar\n2)Recorrer\n3)Editar\n4)Eliminar\n5)Ordenar\n6)Salir\n\nOpcion: ";
+		system("cls");
+		cout<<"  1)Agregar\n  2)Recorrer\n  3)Editar\n  4)Eliminar\n  5)Ordenar\n  6)Salir\n\n  Opcion: ";
 		cin>>opcion;
+		system("cls");
 		switch(opcion){
 			case 1:
-				registrar(carro);
-				list->agregarNodo(carro);
+				list->agregarCarro();
 				break;
 			case 2:
 				list->recorrerLista();
@@ -201,7 +184,7 @@ int main(){
 			case 5:
 				break;
 			case 6:
-				cout<<endl<<"Saliste del programa"<<endl;
+				cout<<endl<<"  Saliste del programa"<<endl;
 				break;
 		}
 	}while(opcion!=6);		
@@ -210,22 +193,30 @@ int main(){
 void registrar(Carro *c){
 	string a, b, f;
 	int d, e;
-	cout<<endl<<"Introduce la marca del vehiculo: ";
+	cout<<endl<<"  Introduce la marca del vehiculo: ";
 	fflush(stdin);getline(cin,a);
 	c->setmarca(a);
-	cout<<endl<<"Introduce el modelo del vehiculo: ";
+	cout<<endl<<"  Introduce el modelo del vehiculo: ";
 	fflush(stdin);getline(cin, b);
 	c->setmodelo(b);
-	cout<<endl<<"Introduce el motor del vehiculo: ";
+	cout<<endl<<"  Introduce el motor del vehiculo: ";
 	fflush(stdin);getline(cin, f);
 	c->setmotor(f);
-	cout<<endl<<"Introduce la capacidad de pasajeros del vehiculo: ";
+	cout<<endl<<"  Introduce la capacidad de pasajeros del vehiculo: ";
 	cin>>e;
 	c->setcapacidad(e);
-	cout<<endl<<"Introduce el numero de llantas del vehiculo: ";
+	cout<<endl<<"  Introduce el numero de llantas del vehiculo: ";
 	cin>>d;
 	c->setnumLlantas(d);
 	cout<<endl<<endl;
 }
 
+void mostrar(Carro *aux){
+			cout<<endl<<"  Marca: "<<aux->getmarca()<<endl;
+			cout<<"  Modelo: "<<aux->getmodelo()<<endl;
+			cout<<"  Capacidad de pasajeros: "<<aux->getcapacidad()<<endl;
+			cout<<"  Motor: "<<aux->getmotor()<<endl;
+			cout<<"  Numero de llantas: "<<aux->getnumLlantas()<<endl;
+			cout<<endl;
+		}
 
